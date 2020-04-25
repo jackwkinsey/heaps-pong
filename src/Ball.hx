f58677c20c6a6a22e97a76e0e71ebc58b523f3cd
Jack Kinsey <jack_kinsey@sweetwater.com>
2020-04-24 22:28:02 -0400

    feat: move ball in random direction after cooldown Ball waits 3 seconds then moves in a random left, right, up, down, direction (never straight up or down).

--- src/Ball.hx
+++ src/Ball.hx
@@ -1,22 +1,22 @@
-import h2d.Scene;
+import h2d.Object;
 import h2d.Tile;
 import h2d.Bitmap;
 
-class Ball {
-	var s2d:Scene;
-	var x:Float;
-	var y:Float;
+class Ball extends Object {
 	var speed:Float;
 	var spawnTimer:Float;
 	var bmp:Bitmap;
 
-	public function new(x:Float, y:Float, size:Int, ?speed:Float = 1, s2d:Scene) {
+	public function new(x:Float, y:Float, size:Int, ?moveRight:Bool = true, ?speed:Float = 1, ?parent:Object) {
+		super(parent);
 		this.x = x;
 		this.y = y;
 		this.speed = speed;
-		this.s2d = s2d;
-		this.CreateGraphic(0xFFFFFF, size);
-		this.spawnTimer = 3;
+		spawnTimer = 3;
+		CreateGraphic(0xFFFFFF, size, parent);
+
+		var moveUp:Bool = Math.floor(Math.random() * 2) > 0;
+		ChooseDirection(moveRight, moveUp);
 	}
 
 	public function update(dt:Float) {
@@ -24,18 +24,39 @@ class Ball {
 			spawnTimer -= dt;
 			return;
 		}
+
+		move(speed, -speed);
+		bmp.x = x;
+		bmp.y = y;
 	}
 
-	private function CreateGraphic(color:Int, width:Int, ?height:Int) {
+	private function CreateGraphic(color:Int, width:Int, ?height:Int, ?parent:Object) {
 		if (height == null) {
 			height = width;
 		}
 
 		var tile = Tile.fromColor(color, width, height);
-		bmp = new Bitmap(tile, this.s2d);
-		bmp.x = this.x;
-		bmp.y = this.y;
+		bmp = new Bitmap(tile, parent);
+		bmp.x = x;
+		bmp.y = y;
 		bmp.tile.dx = -width / 2;
 		bmp.tile.dy = -height / 2;
+	}
+
+	private function ChooseDirection(moveRight:Bool, moveUp:Bool) {
+		var degrees:Float = 0;
+		var randomNumber = Math.floor(Math.random() * 45);
+
+		if (moveRight) {
+			degrees = randomNumber;
+		} else {
+			degrees = randomNumber + 135;
+		}
+
+		rotation = degrees * Math.PI / 180;
+
+		if (!moveUp) {
+			rotation = -rotation;
+		}
 	}
 }
--- src/Main.hx
+++ src/Main.hx
@@ -18,7 +18,9 @@ class Main extends App {
 
 		s2d.addChild(tf);
 
-		this.ball = new Ball(s2d.width / 2, s2d.height / 2, 10, s2d);
+		// @todo left and right direction should be based on which player last scored; only pick random direction when score is 0-0.
+		var moveRight:Bool = Math.floor(Math.random() * 2) > 0;
+		this.ball = new Ball(s2d.width / 2, s2d.height / 2, 10, moveRight, 4, s2d);
 	}
 
 	override function update(dt:Float) {
